f0a61138b3ee70d74670dada7c685226671cb023
JDSanto <javidisanto@gmail.com>
2020-12-10 22:59:40 -0300

    Ejercicio 5.6

---
+++ 5.6/5.6-dual.sol
@@ -0,0 +1,42 @@
+Problem:    5
+Rows:       5
+Columns:    5
+Non-zeros:  15
+Status:     OPTIMAL
+Objective:  z = 550 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 COND1        B           16.5            10               
+     2 COND2        NL            15            15                     13.3333 
+     3 COND3        NL            15            15                     3.33333 
+     4 COND4        NL            18            18                     16.6667 
+     5 z            B            550                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 Y_1          B            2.5             0               
+     2 Y_2          B           3.75             0               
+     3 Y_3          B            2.5             0               
+     4 Y_4          NL             0             0                           6 
+     5 Y_5          NL             0             0                     6.66667 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
