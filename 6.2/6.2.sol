2f0e278fe1583a00a8c4361d454394a8df5dbb05
JDSanto <javidisanto@gmail.com>
2021-02-25 17:00:15 -0300

    Ejercicio 6.2, correcciones 6.2 y 6.16

---
+++ 6.2/6.2.sol
@@ -0,0 +1,39 @@
+Problem:    6
+Rows:       4
+Columns:    3
+Non-zeros:  10
+Status:     OPTIMAL
+Objective:  z = 30 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 z            B             30                             
+     2 MP           B             20                          36 
+     3 HM           NU            20                          20           1.5 
+     4 DM           B             10             2               
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 X_1          NL             0             0                          -2 
+     2 X_2          B             10             0               
+     3 X_3          NL             0             0                          -1 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
