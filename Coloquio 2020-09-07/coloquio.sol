25cd9b793c6e6dcb0607e2d1d395df6a9bcdf7f2
JDSanto <javidisanto@gmail.com>
2021-03-22 08:51:24 -0300

    Agrega coloquios

---
+++ Coloquio 2020-09-07/coloquio.sol
@@ -0,0 +1,38 @@
+Problem:    coloquio
+Rows:       4
+Columns:    2
+Non-zeros:  7
+Status:     OPTIMAL
+Objective:  z = 8000 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 R1           NU           800                         800            10 
+     2 R2           B           -400                         200 
+     3 R3           B            400           100               
+     4 z            B           8000                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 X1           NL             0             0                          -1 
+     2 X2           B            400             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
---
+++ Coloquio 2020-09-21/coloquio.sol
@@ -0,0 +1,38 @@
+Problem:    coloquio
+Rows:       4
+Columns:    2
+Non-zeros:  7
+Status:     OPTIMAL
+Objective:  z = 2050 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 R1           NU            80                          80          22.5 
+     2 R2           NU            50                          50             5 
+     3 R3           B             10            10               
+     4 z            B           2050                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 X1           B             30             0               
+     2 X2           B             10             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
